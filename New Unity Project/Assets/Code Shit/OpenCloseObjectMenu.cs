e616868aa2c8b06f5f12225bd7f592f7eba796df
Alex Huntoon <ahuntoon@tripadvisor.com>
2016-01-30 17:22:08 -0500

    Before building import

---
+++ New Unity Project/Assets/Code Shit/OpenCloseObjectMenu.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenCloseObjectMenu : MonoBehaviour {
+
+	public GameObject menuItemTop;
+	public Camera playerCamera;
+
+	// Use this for initialization
+	void Start () {
+		print ("HelloInit");
+	}
+
+	// Update is called once per frame
+
+	void Update () {
+		Ray ray = playerCamera.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Input.GetMouseButtonDown(0)) {
+			if (Physics.Raycast(ray, out hit)){
+				print ("Hello");
+				//menuItemTop.GetComponent<Renderer> ().enabled = !menuItemTop.GetComponent<Renderer>().enabled;
+				//print (menuItemTop.activeInHierarchy);
+				menuItemTop.SetActive (!menuItemTop.activeInHierarchy);
+				/*
+				foreach (GameObject c in menuItems) {
+					print (c.activeInHierarchy);
+					c.SetActive (!c.activeInHierarchy);
+				}
+				*/
+			}
+		}
+		Component halo = this.GetComponent ("Halo");
+		if (Physics.Raycast (ray, out hit)) {
+			halo.GetType ().GetProperty ("enabled").SetValue (halo, true, null);
+		} else {
+			halo.GetType ().GetProperty ("enabled").SetValue (halo, false, null);
+		}
+	}
+}
